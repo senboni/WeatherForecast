bebb994f8b22d2b25c19098226d36108191288ed
Senboni <kajler11@gmail.com>
2024-10-04 13:23:41 +0200

    add cached provider unit tests

---
+++ test/WeatherForecast.UnitTests/WeatherProviders/OpenWeatherMapWeatherProviderCachedTest.cs
@@ -0,0 +1,70 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Caching.Memory;
+using NSubstitute;
+using System.Net;
+using WeatherForecast.Host.Common;
+using WeatherForecast.Host.WeatherProviders;
+
+namespace WeatherForecast.UnitTests.WeatherProviders;
+
+public class OpenWeatherMapWeatherProviderCachedTest
+{
+    [Fact]
+    public async Task GetCurrentWeatherCalledTwice_ShouldCallHttpClientOnce()
+    {
+        //arrange
+        var httpClientFactory = Substitute.For<IHttpClientFactory>();
+        var httpClient = new HttpClient(new MockHandler())
+        {
+            BaseAddress = new Uri("https://mockurl.com"),
+        };
+
+        httpClientFactory.CreateClient(Constants.OpenWeatherMapClient).Returns(httpClient);
+
+        var cache = new MemoryCache(new MemoryCacheOptions());
+        var provider = new OpenWeatherMapWeatherProvider(httpClientFactory);
+        var cachedProvider = new OpenWeatherMapWeatherProviderCached(provider, cache);
+
+        //act
+        await cachedProvider.GetCurrentWeather<Result<object, HttpStatusCode>>("Barcelona", "C", default);
+        await cachedProvider.GetCurrentWeather<Result<object, HttpStatusCode>>("Barcelona", "C", default);
+
+        //assert
+        httpClientFactory.Received(1).CreateClient(Constants.OpenWeatherMapClient);
+    }
+
+    [Fact]
+    public async Task GetForecastWeatherCalled3Times_ShouldCallHttpClientOnce()
+    {
+        //arrange
+        var httpClientFactory = Substitute.For<IHttpClientFactory>();
+        var httpClient = new HttpClient(new MockHandler())
+        {
+            BaseAddress = new Uri("https://mockurl.com"),
+        };
+
+        httpClientFactory.CreateClient(Constants.OpenWeatherMapClient).Returns(httpClient);
+
+        var cache = new MemoryCache(new MemoryCacheOptions());
+        var provider = new OpenWeatherMapWeatherProvider(httpClientFactory);
+        var cachedProvider = new OpenWeatherMapWeatherProviderCached(provider, cache);
+
+        //act
+        await cachedProvider.GetForecastWeather<Result<object, HttpStatusCode>>("Barcelona", "C", default);
+        await cachedProvider.GetForecastWeather<Result<object, HttpStatusCode>>("Barcelona", "C", default);
+        await cachedProvider.GetForecastWeather<Result<object, HttpStatusCode>>("Barcelona", "C", default);
+
+        //assert
+        httpClientFactory.Received(1).CreateClient(Constants.OpenWeatherMapClient);
+    }
+
+    private class MockHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            => Task.FromResult(new HttpResponseMessage
+            {
+                Content = new StringContent("{\"someField\": \"someValue\"}"),
+                StatusCode = HttpStatusCode.OK,
+            });
+    }
+}
